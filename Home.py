24199e659867a8a0650168fc51693cbef598041f
CSW <dummy.lab@hotmail.com>
2024-09-19 16:07:10 +0200

    changes

--- Home.py
+++ Home.py
@@ -1,10 +1,12 @@
 import streamlit as st
+import json
 from PIL import Image
 from io import BytesIO
 import vertexai
 from vertexai.preview.vision_models import ImageGenerationModel
 import time
 from google.api_core.exceptions import ResourceExhausted
+from streamlit_lottie import st_lottie_spinner, st_lottie
 
 
 ### project details ###
@@ -35,15 +37,6 @@ sub_body2 = st.container()
 
 ### main page header ###
 with header: 
-    st.markdown("""
-            <h1 style='text-align: center; 
-            font-family: Times New Roman; 
-            font-size: 35px; 
-            color: #ff6200;'>
-            The BEST Image Generator EVER
-            </h1>
-            """, unsafe_allow_html=True)
-
     st.image('ing_icon/imagin.png', width=600)
 
 ### button customize ###
@@ -64,86 +57,114 @@ st.markdown("""
         color: white;
         border:1px solid #ff8533;
     }
+    div.stButton > button:first-child:active {
+        background-color: white;
+        color: #ff8533;
+        border:1px solid #ff8533;
+    }
     </style>
     """, unsafe_allow_html=True)
+
+### animations ### 
+path1 = "animations/spinner.json"
+with open(path1,"r") as file: 
+    spinner_url = json.load(file) 
+
+path2 = "animations/confetti.json"
+with open(path2,"r") as file2: 
+    confetti_url = json.load(file2) 
 
 
 ### main page body ###
 with body: 
     user_prompt = st.text_input('Please type in your prompt')
-    option = st.selectbox('Please select the setting', ['Office', 'Outdoors'])
+    option = st.selectbox('Please select the setting', ['Office', 'Customer', 'Illustration'])
 
     if st.button('Generate Image'):
-        if option == 'Office':
-            system_prompt = """
-                            Setting:
-                            diverse, happy, office setting, professional photography, high quality, colorful, energetic, orange elements, smiling, playful, funny situation, sustainable, fair society, harmony, team spirit, working together
-                            Image Content:
-                            """
-        
-        
-        elif option == 'Outdoors':
-            system_prompt = """
-                            Setting:
-                            diverse, happy, outdoor setting, professional photography, high quality, colorful, energetic, orange elements, smiling, playful, funny situation, sustainable, fair society, harmony, team spirit, working together
-                            Image Content:
-                            """
+        with st_lottie_spinner(spinner_url, reverse=True, speed=1, loop=True, quality='high', height=200, width=200, key='spinner1'):
 
-        negative_prompt = """
-            out of frame, lowres, text, cropped, worst quality, low quality, jpeg artifacts, duplicate, mutilated, extra fingers, mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, bad anatomy, bad proportions, extra limbs, cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, fused fingers, too many fingers, long neck, watermark, signature, brands, violence, weapons, blood
-            """
-        
-        prompt = system_prompt + user_prompt
+            if option == 'Office':
+                system_prompt = """
+                                Setting:
+                                diverse, happy, office setting, professional photography, high quality, colorful, energetic, orange elements, 
+                                smiling, playful, funny situation, sustainable, fair society, harmony, team spirit, working together
+                                Image Content:
+                                """
+            
+            elif option == 'Customer':
+                system_prompt = """
+                                Setting:
+                                happy, fun, diverse, action, enjoying, natural, snapshot, daily life, energetic, playful, harmony, freedom
+                                Image Content:
+                                """
 
-        success = False
+            elif option == 'Illustration':
+                system_prompt = """
+                                Setting:
+                                flat design illustraton, minimalistic, simple geometric shapes, happy, fun, diverse, simple, smooth rounded edges,
+                                non-realistic, no complex details on texture, white background
+                                Image Content:
+                                """
 
-        while not success:
-            try:
-                response = generation_model.generate_images(
-                    prompt=prompt,
-                    number_of_images=4,
-                    aspect_ratio="4:3",
-                    safety_filter_level="block_some",
-                    person_generation="allow_adult", 
-                    negative_prompt=negative_prompt
-                )
+            negative_prompt = """
+                out of frame, lowres, text, cropped, worst quality, low quality, jpeg artifacts, duplicate, mutilated, extra fingers, 
+                mutated hands, poorly drawn hands, poorly drawn face, mutation, deformed, blurry, bad anatomy, bad proportions, extra limbs, 
+                cloned face, disfigured, gross proportions, malformed limbs, missing arms, missing legs, extra arms, extra legs, 
+                fused fingers, too many fingers, long neck, watermark, signature, brands, violence, weapons, blood, nudity
+                """
+            
+            prompt = system_prompt + user_prompt
 
-            except ResourceExhausted:
-                print("Resource is exhausted, trying again in 2 seconds")
-                time.sleep(2)
+            success = False
 
-            except Exception as e:
-                st.error(f"Error generating image: {e}")
+            while not success:
+                try:
+                    response = generation_model.generate_images(
+                        prompt=prompt,
+                        number_of_images=4,
+                        aspect_ratio="4:3",
+                        safety_filter_level="block_some",
+                        person_generation="allow_adult", 
+                        negative_prompt=negative_prompt
+                    )
 
-            else:
-                success = True
+                except ResourceExhausted:
+                    print("Resource is exhausted, trying again in 2 seconds")
+                    time.sleep(2)
 
-                images = []
+                except Exception as e:
+                    st.error(f"Error generating image: {e}")
 
-                for index in range(len(list(response))):
-                    image_data = response.images[index]._image_bytes
-                    image = Image.open(BytesIO(image_data))
-                    images.append(image)
+                else:
+                    success = True
 
-                with sub_body1: 
-                    img1, img2 = st.columns(2)
-                
-                    with img1:
-                        st.image(images[0], caption='Image 1', use_column_width=True)
+                    images = []
+
+                    for index in range(len(list(response))):
+                        image_data = response.images[index]._image_bytes
+                        image = Image.open(BytesIO(image_data))
+                        images.append(image)
+
+                    with sub_body1: 
+                        img1, img2 = st.columns(2)
                     
-                    with img2:
-                        st.image(images[1], caption='Image 2', use_column_width=True)
+                        with img1:
+                            st.image(images[0], caption='Image 1', use_column_width=True)
+                        
+                        with img2:
+                            st.image(images[1], caption='Image 2', use_column_width=True)
 
-                with sub_body2: 
-                    img3, img4 = st.columns(2)
+                    with sub_body2: 
+                        img3, img4 = st.columns(2)
 
-                    with img3:
-                        st.image(images[2], caption='Image 3', use_column_width=True)
+                        with img3:
+                            st.image(images[2], caption='Image 3', use_column_width=True)
 
-                    with img4:
-                        st.image(images[3], caption='Image 4', use_column_width=True)
+                        with img4:
+                            st.image(images[3], caption='Image 4', use_column_width=True)
 
-                
+                    st_lottie(confetti_url, reverse=True, speed=1, loop=False,
+                                           quality='high', height=200, width=200, key='confetti1')
 
 
 
